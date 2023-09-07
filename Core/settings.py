d403a2078419ebf315dd8caf98cf2b39a652703c
PolarV-UNIX <hamid.hosseny2@gmail.com>
2023-09-07 13:52:12 -0500

    complete register with JWT

--- Core/settings.py
+++ Core/settings.py
@@ -44,7 +44,25 @@ INSTALLED_APPS = [
     'rest_framework',
     
     #Apps
+    'User',
 ]
+
+# DRF JWT TOKEN CONFIG
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # OTHER AUTH CLASS IF NEEDED
+    )
+}
+# ACCESS TOKEN EXPIRE
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=60),  # Set the access token lifetime to 60 days
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=60),  # Optional: Set the refresh token lifetime to 60 days
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=120),  # Optional: Set the sliding token lifetime to 120 days
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -74,6 +92,14 @@ TEMPLATES = [
     },
 ]
 
+# CORS SETTINGS
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
+
 WSGI_APPLICATION = 'Core.wsgi.application'
 
 
@@ -85,9 +111,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         # gitignore-start
-        'NAME': '', #Define name of DB
-        'USER': '', #Define name of root user DB
-        'PASSWORD': '',
+        'NAME': 'ecommerce', #Define name of DB
+        'USER': 'root', #Define name of root user DB
+        'PASSWORD': 'H3981331030h',
         # gitignore-end
         'HOST': 'localhost',
         'PORT': '3306',
@@ -128,6 +154,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# AUTH CUSTOM USER MODEL
+AUTH_USER_MODEL = 'User.User'
+
+# GET  USER URL
+GET_USER_URL = ''
+
+# DEFINE AUTH BACKEND
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'Core.auth_backend.MyBackend',
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
